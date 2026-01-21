9a82b22a6d0f38fa657d9e72843bccfadfcd10d3
Jonah Jacobsen <jonah.jacobsen@arcserve.com>
2026-01-20 20:28:48 -0600

    Place script in normal PATH dir, not /root/

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-cp runner-resource-caps.sh /root/
+cp runner-resource-caps.sh /usr/local/bin/
 cp runner-resource-caps.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable runner-resource-caps.service
